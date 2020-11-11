e53694274227706bda63df5e12495242f29a9e9f
uch1104 <foajoaquin@gmail.com>
2020-11-11 13:55:09 +0900

    ユーザー機能のエラー画面の修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module CampOut
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +19,5 @@ module CampOut
     # the framework and any gems in your application.
   end
 end
+
+
