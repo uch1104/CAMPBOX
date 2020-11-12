8d4d3bd41abe359c1b49ccf63e920c9dacca2d0c
uch1104 <foajoaquin@gmail.com>
2020-11-12 18:28:23 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_method
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :start_date
+    validates :limit_date
+    validates :price, format: { with: /\A[0-9]+\z/ }
+  end
+  validates :price, inclusion: { in: (300..9_999_999) }
+  validates :condition_id, :cost_id, :prefecture_id, :shipping_method_id, numericality: { other_than: 1 }
 end
