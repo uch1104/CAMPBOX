a0148a74b91048220541673b151eed66508e3853
uch1104 <foajoaquin@gmail.com>
2020-11-13 15:20:23 +0900

    Active Hashのname変更

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,6 +1,6 @@
 class ShippingMethod < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '選択してください' },
     { id: 2, name: '宅急便' },
     { id: 3, name: '直接手渡し' }
   ]
