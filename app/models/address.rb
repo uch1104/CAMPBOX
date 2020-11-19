a2e44209e21b8886b785bf25b4c39934f49ed5db
uch1104 <foajoaquin@gmail.com>
2020-11-19 19:15:31 +0900

    addressモデルの単体テストコード作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,7 @@ class Address < ApplicationRecord
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ }
   end
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
