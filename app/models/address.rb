c842780db9add7da02e9cad638d5d08147259763
uch1104 <foajoaquin@gmail.com>
2020-11-19 13:27:33 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,14 @@
 class Address < ApplicationRecord
   belongs_to :user
 
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
