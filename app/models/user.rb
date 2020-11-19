1754a1212c1c11feb71637f8046c14ab47bad960
uch1104 <foajoaquin@gmail.com>
2020-11-19 12:50:10 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   has_many :items
   has_one :card, dependent: :destroy
   has_many :orders
+  has_one :address, dependent: :destroy
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
